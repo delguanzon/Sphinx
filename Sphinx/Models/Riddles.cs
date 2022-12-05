27a6169d90a7cc640fd390c75d5376b2f4073d1a
delguanzon <yodelguanzon@gmail.com>
2022-12-05 02:48:58 -0800

    Add project files.

---
+++ Sphinx/Models/Riddles.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sphinx.Models
+{
+    public class Riddles
+    {
+
+        private Dictionary<string, string> _riddlePairs = new Dictionary<string, string>() {
+            { "What gets wet while drying?", "towel" },
+            { "What can you catch, but not throw?", "cold" },
+            { "What is red and smells like blue paint?", "red paint" },
+            { "What runs, but never walks. Murmurs, but never talks. Has a bed, but never sleeps. And has a mouth, but never eats" , "river" }
+        };
+
+        private string _currentAnswer;
+
+        public string GetRiddle()
+        {
+            return RandomRiddle();
+        }
+
+        public void AddRiddle(string question, string answer)
+        {
+            _riddlePairs[question] = answer;
+        }
+
+        private string RandomRiddle() 
+        {
+            Random rnd = new Random();
+            int i = rnd.Next(0, _riddlePairs.Count - 1);
+            string riddle = _riddlePairs.ElementAt(i).Key;
+            _currentAnswer = _riddlePairs.ElementAt(i).Value;
+
+            return riddle;
+        }
+        public bool checkAnswer(string answer)
+        {
+            bool check = answer.ToLower().Trim().Contains(_currentAnswer);
+            return check;
+        }
+    }
+}
---
+++ Sphinx/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Sphinx.Models;
+
+public class Program
+{
+    public static void Main()
+    {
+        Riddles riddle = new Riddles();
+        int tries = 0;
+        Console.WriteLine("Sphinx: Welcome! In order to proceed you must answer my Riddle.");
+        Console.WriteLine("Sphinx: You'll get three tries.");
+        
+        Console.WriteLine(riddle.GetRiddle());
+
+        
+
+        for(int ctr = 1; ctr <= 3 ; ctr++)
+        {
+            Console.WriteLine("{Sphinx: Answer me these:");
+            string answer = Console.ReadLine();
+            if (riddle.checkAnswer(answer))
+            {
+                Console.WriteLine("Sphinx: Congrats! Your answer, {0} , is correct", answer);
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Sphinx: WRONG ! That's {0} out of 3", ctr);
+                
+            }
+            tries = ctr;
+        }
+
+        if (tries == 3)
+        {
+            Console.WriteLine("\nSphinx: YOU SHALL NOT PASS");
+        }
+        else
+        {
+            Console.WriteLine("Sphinx: YOU SHALL PASS!");
+            Console.WriteLine("Sphinx: Unless you want another riddle?[Y][N]");
+            string answer = Console.ReadLine();
+            if(answer.Trim().ToLower() == "y")
+            {
+                Main();
+            }
+        }
+
+    }
+}
